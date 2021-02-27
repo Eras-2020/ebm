a4548e192a33f4c9d8035e8217ed162bfeb8f8ed
Erastus <66550159+Eras-2020@users.noreply.github.com>
2021-02-27 15:28:41 +0300

    Update settings.py

--- ebm/settings.py
+++ ebm/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nzulaerastus@gmail.com'
-EMAIL_HOST_PASSWORD = 'Era@2020'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
